3bf2c74aa8a39b68dd489c5dabadd357374e7a0c
Furqan Khan <160186238+furqanalikhan1@users.noreply.github.com>
2024-03-16 01:24:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//     BMI Calcolator  in type scripet
+var weight = 87;
+// let height = 5.6;
+var bmi = weight;
+console.log("furqan  your BMI is ".concat(bmi));
+// /height*height
---
+++ main.ts
@@ -0,0 +1,8 @@
+//     BMI Calcolator  in type scripet
+
+let weight = 87;
+let height = 5.6;
+
+
+let bmi = weight/height*height
+console.log(`furqan  your BMI is ${bmi}`)
